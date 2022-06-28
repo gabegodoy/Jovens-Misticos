ececfded00f164a8d2274133b3b15cc108c13461
Gabriel Godoy <gabe.profe@gmail.com>
2022-06-28 17:09:24 -0300

    quote desktop and mobile ready

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 const homePage = document.querySelector('.home__container');
 const loadPage = document.querySelector('.loading__container');
+const quotePage = document.querySelector('.quote__container')
 
 
 function load(){
   homePage.style.display = 'none'
   loadPage.style.display = 'flex'
-  
+  setTimeout(loadQuote, 4000)
 }
 
-
+function loadQuote(){
+  loadPage.style.display = 'none'
+  quotePage.style.display = 'flex'
+}
 
 
