4efca7d6fc0e84611b35cab1f8dca6e90116f0e5
Gabriel Godoy <gabe.profe@gmail.com>
2022-06-28 15:48:18 -0300

    loading page desktop ready

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const homePage = document.querySelector('.home__container');
+const loadPage = document.querySelector('.loading__container');
+
+
+function load(){
+  homePage.style.display = 'none'
+  loadPage.style.display = 'flex'
+  
+}
+
+
+
+
