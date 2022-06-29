316b679e498a9247cadba5a0c1a9fa2fd9fadb67
Gabriel Godoy <gabe.profe@gmail.com>
2022-06-28 21:47:29 -0300

    update de frases

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 const homePage = document.querySelector('.home__container');
 const loadPage = document.querySelector('.loading__container');
 const quotePage = document.querySelector('.quote__container')
+const quoteText = document.querySelector('.quote__text__sentence')
+const quoteAuthor = document.querySelector('.quote__text__author')
 
+function getRandomArbitrary(min, max) {
+ return Math.random() * (max - min) + min;
+}
+let randomNum = parseInt(getRandomArbitrary(1, 306))
 
 function load(){
   homePage.style.display = 'none'
@@ -12,6 +18,17 @@ function load(){
 function loadQuote(){
   loadPage.style.display = 'none'
   quotePage.style.display = 'flex'
+
+  quoteText.appendChild(document.createTextNode(frases[randomNum].estrofe))
+  quoteAuthor.appendChild(document.createTextNode(frases[randomNum].poeta))
 }
 
 
+
+/*  */
+
+/*
+
+console.log(randomNum)
+console.log(frases[randomNum].estrofe)
+console.log(frases[randomNum].poeta) */
