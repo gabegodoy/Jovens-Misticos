b602acdb348b4e2097265825b326ca7b12478de6
Gabriel Godoy <gabe.profe@gmail.com>
2022-06-28 22:18:17 -0300

    re-load button on quote screen

--- js/main.js
+++ js/main.js
@@ -7,28 +7,32 @@ const quoteAuthor = document.querySelector('.quote__text__author')
 function getRandomArbitrary(min, max) {
  return Math.random() * (max - min) + min;
 }
-let randomNum = parseInt(getRandomArbitrary(1, 306))
+let randomNum;
 
 function load(){
   homePage.style.display = 'none'
   loadPage.style.display = 'flex'
+  quotePage.style.display = 'none'
+
   setTimeout(loadQuote, 4000)
 }
 
 function loadQuote(){
+  randomNum = parseInt(getRandomArbitrary(1, 306))
+
   loadPage.style.display = 'none'
   quotePage.style.display = 'flex'
 
+
+  quoteText.innerHTML = ''
+  quoteAuthor.innerHTML = ''
   quoteText.appendChild(document.createTextNode(frases[randomNum].estrofe))
   quoteAuthor.appendChild(document.createTextNode(frases[randomNum].poeta))
 }
 
+function newQuote(){
+
+  
+}
 
 
-/*  */
-
-/*
-
-console.log(randomNum)
-console.log(frases[randomNum].estrofe)
-console.log(frases[randomNum].poeta) */
