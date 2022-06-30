5c1715ea9e3353440aa90696489b33b21208ea63
Gabriel Godoy <gabe.profe@gmail.com>
2022-06-30 12:33:40 -0300

    english API integration

--- js/main.js
+++ js/main.js
@@ -3,13 +3,42 @@ const loadPage = document.querySelector('.loading__container');
 const quotePage = document.querySelector('.quote__container')
 const quoteText = document.querySelector('.quote__text__sentence')
 const quoteAuthor = document.querySelector('.quote__text__author')
+let author;
+let quote;
 
+/* API GET */
+
+function getApi (){
+  const options = {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'defaut'
+  }
+
+  fetch('https://api.quotable.io/random')
+
+  .then((reponse) => {reponse.json()
+    .then(data => {
+      author = data.author;
+      quote = data.content;   
+    
+    }) })
+    .catch(e => console.log('Error' + e))
+
+    
+}
+
+
+/* 
 function getRandomArbitrary(min, max) {
  return Math.random() * (max - min) + min;
 }
 let randomNum;
+ */
 
 function load(){
+  getApi ();
+
   homePage.style.display = 'none'
   loadPage.style.display = 'flex'
   quotePage.style.display = 'none'
@@ -18,7 +47,7 @@ function load(){
 }
 
 function loadQuote(){
-  randomNum = parseInt(getRandomArbitrary(1, 306))
+  /* randomNum = parseInt(getRandomArbitrary(1, 306)) */
 
   loadPage.style.display = 'none'
   quotePage.style.display = 'flex'
@@ -26,8 +55,8 @@ function loadQuote(){
 
   quoteText.innerHTML = ''
   quoteAuthor.innerHTML = ''
-  quoteText.appendChild(document.createTextNode(frases[randomNum].estrofe))
-  quoteAuthor.appendChild(document.createTextNode(frases[randomNum].poeta))
+  quoteText.appendChild(document.createTextNode(quote))
+  quoteAuthor.appendChild(document.createTextNode(author))
 }
 
 function newQuote(){
